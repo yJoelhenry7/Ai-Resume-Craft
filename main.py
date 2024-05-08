2c24ba7150e7ebd07e892085f6f3b3a02f32bf19
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2024-05-08 18:51:57 +0530

    adding all the required files to get started

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI,Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    message = "Hello"
+    return templates.TemplateResponse("home.html", { "request" : request,"message": message})
